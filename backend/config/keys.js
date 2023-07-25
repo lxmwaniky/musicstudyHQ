e1c1d1a2ffacdf7f9c1cfa8a37fa471208cfdb7f
Braine Lomoni <kapolonbraine@gmail.com>
2023-07-25 21:04:04 +0300

    feat : Extracted keys from .env file with credentials.

---
+++ backend/config/keys.js
@@ -0,0 +1,11 @@
+import dotnev from "dotnev"
+dotnev.config();
+
+export const KEYS = {
+    PORT: process.env.PORT || 8000,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME
+}
